a11043aad204a703525f740bb257a4980dea920b
Yasin Asadnezhad <yasin.asadnezhad@msn.com>
2023-07-21 03:02:41 +0330

    Added MediatR base controller

---
+++ src/Mc2.CrudTest.Presentation/Server/Controllers/BaseApiController.cs
@@ -0,0 +1,16 @@
+﻿ 
+using MediatR;
+
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Mc2.CrudTest.Presentation.Server.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public abstract class BaseApiController : ControllerBase
+{
+    private IMediator _mediator;      
+    protected IMediator Mediator => _mediator ??= HttpContext.RequestServices.GetService<IMediator>();
+     
+}
---
+++ src/Mc2.CrudTest.Presentation/Server/Controllers/CustomerController.cs
@@ -0,0 +1,16 @@
+﻿ 
+
+using MediatR;
+
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Mc2.CrudTest.Presentation.Server.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class CustomerController : BaseApiController
+{
+  
+   
+}
