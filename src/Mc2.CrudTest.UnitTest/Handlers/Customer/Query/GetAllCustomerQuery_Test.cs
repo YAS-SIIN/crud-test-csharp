748a02ee0e36125f567a342334fd3df3391917c4
Yasin Asadnezhad <yasin.asadnezhad@msn.com>
2023-07-22 22:40:51 +0330

    Coded Get All and Get customer DDT

---
+++ src/Mc2.CrudTest.UnitTest/Handlers/Customer/Query/GetAllCustomerQuery_Test.cs
@@ -0,0 +1,34 @@
+﻿
+using FluentValidation;
+
+using Mc2.CrudTest.Application.Application.UseCases.Customers.Queries;
+using Mc2.CrudTest.Application.UseCases.Customer.Commands;
+using Mc2.CrudTest.Core.Commands.Customer;
+using Mc2.CrudTest.Core.Queries.Customer;
+using Mc2.CrudTest.Domain.DTOs.Exceptions;
+using Mc2.CrudTest.Domain.Enums;
+ 
+namespace Mc2.CrudTest.UnitTest.Handlers.Customer.Query;
+
+public class GetAllCustomerQuery_Test
+{
+    private readonly GetAllCustomerQueryHandler _getAllCustomerQueryHandler;
+    public GetAllCustomerQuery_Test()
+    {
+
+        TestTools.Initialize();
+        _getAllCustomerQueryHandler = new GetAllCustomerQueryHandler(TestTools.mockUnitOfWork.Object);
+
+    }
+
+    [Fact]
+    public async Task GetAllCustomer_ShouldBeSucceeded()
+    { 
+        var requestData = new GetAllCustomerQuery();
+        var responseData = await _getAllCustomerQueryHandler.Handle(requestData, CancellationToken.None);
+
+        Assert.NotNull(responseData.Data);
+        Assert.NotEqual(0, responseData.Data.Count);
+    }
+     
+}
---
+++ src/Mc2.CrudTest.UnitTest/Handlers/Customer/Query/GetCustomerQuery_Test.cs
@@ -0,0 +1,44 @@
+﻿
+using FluentValidation;
+
+using Mc2.CrudTest.Application.Application.UseCases.Customers.Queries;
+using Mc2.CrudTest.Application.UseCases.Customer.Commands;
+using Mc2.CrudTest.Core.Commands.Customer;
+using Mc2.CrudTest.Core.Queries.Customer;
+using Mc2.CrudTest.Domain.DTOs.Exceptions;
+using Mc2.CrudTest.Domain.Enums;
+ 
+namespace Mc2.CrudTest.UnitTest.Handlers.Customer.Query;
+
+public class GetCustomerQuery_Test
+{
+    private readonly GetCustomerQueryHandler _getCustomerQueryHandler;
+    public GetCustomerQuery_Test()
+    {
+
+        TestTools.Initialize();
+        _getCustomerQueryHandler = new GetCustomerQueryHandler(TestTools.mockUnitOfWork.Object);
+
+    }
+
+    [Theory]
+    [InlineData(1)]
+    public async Task GetCustomer_ShouldBeSucceeded(int id)
+    { 
+        var requestData = new GetCustomerQuery { Id = id };
+        var responseData = await _getCustomerQueryHandler.Handle(requestData, CancellationToken.None);
+
+        Assert.NotNull(responseData.Data);
+    }
+
+    [Theory]
+    [InlineData(5)]
+    public async Task GetCustomer_ShouldBeFailed(int id)
+    {
+
+        var requestData = new GetCustomerQuery { Id = id };
+
+        await Assert.ThrowsAsync<ErrorException>(() => _getCustomerQueryHandler.Handle(requestData, CancellationToken.None));
+    }
+
+}
